c39c0d0fd7fb3c68cacd54ac9d134768f8c84e87
ssimonrubinstein1@gmail.com <ssimonrubinstein1@gmail.com@37c11c60-c4e7-ad1f-c740-338379875b08>
2012-02-18 14:32:18 +0000

    add propset

--- trunk/lib/Cocoweb.pm
+++ trunk/lib/Cocoweb.pm
@@ -1,3 +1,25 @@
+# @created 2012-02-17
+# @date 2012-02-18
+# @author Simon Rubinstein <ssimonrubinstein1@gmail.com>
+# http://code.google.com/p/cocobot/
+#
+# copyright (c) Simon Rubinstein 2010-2012
+# $Id$
+#
+# cocobot is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# cocobot is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA  02110-1301, USA.
 package Cocoweb;
 
 use strict;
