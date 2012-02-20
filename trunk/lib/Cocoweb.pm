da4d2dd2036934de59dee014a4472da21bad44ee
ssimonrubinstein1@gmail.com <ssimonrubinstein1@gmail.com@37c11c60-c4e7-ad1f-c740-338379875b08>
2012-02-20 20:44:20 +0000

    in progress

--- trunk/lib/Cocoweb.pm
+++ trunk/lib/Cocoweb.pm
@@ -1,5 +1,5 @@
 # @created 2012-02-17
-# @date 2012-02-18
+# @date 2012-02-20
 # @author Simon Rubinstein <ssimonrubinstein1@gmail.com>
 # http://code.google.com/p/cocobot/
 #
@@ -39,6 +39,7 @@ our @EXPORT = qw(
   info
   message
   randum
+  parseInt
 );
 
 sub info {
@@ -62,6 +63,63 @@ sub randum {
     my ($qxq) = @_;
     return floor( rand($qxq) );
 }
+
+## @method int parseInt($str, $radix)
+# @author Father Chrysostomo
+sub parseInt {
+    my ( $str, $radix ) = @_;
+    $str = 'undefined' if !defined $str;
+    my $sign =
+      $str =~ s/^([+-])//
+      ? ( -1, 1 )[ $1 eq '+' ]
+      : 1;
+    $radix = ( int $radix ) % 2**32;
+    $radix -= 2**32 if $radix >= 2**31;
+    $radix ||=
+      $str =~ /^0x/i
+      ? 16
+      : 10;
+    $radix == 16
+      and $str =~ s/^0x//i;
+
+    return if $radix < 2 || $radix > 36;
+
+    my @digits = ( 0 .. 9, 'a' .. 'z' )[ 0 .. $radix - 1 ];
+    my $digits = join '', @digits;
+    $str =~ /^([$digits]*)/i;
+    $str = $1;
+
+    my $ret;
+    if ( !length $str ) {
+        $ret = 'nan';
+    }
+    elsif ( $radix == 10 ) {
+        $ret = $sign * $str;
+    }
+    elsif ( $radix == 16 ) {
+        $ret = $sign * hex $str;
+    }
+    elsif ( $radix == 8 ) {
+        $ret = $sign * oct $str;
+    }
+    elsif ( $radix == 2 ) {
+        $ret = $sign * eval "0b$str";
+    }
+    else {
+        my ( $num, $place );
+        for ( reverse split //, $str ) {
+            $num += (
+                  $_ =~ /[0-9]/
+                ? $_
+                : ord(uc) - 55
+            ) * $radix**$place++;
+        }
+        $ret = $num * $sign;
+    }
+    return $ret;
+}
+
+
 
 sub BEGIN {
     $logger = Cocoweb::Logger->instance();
