967c4e27819f1b022dec989af9623d5ffc2f08b7
ssimonrubinstein1@gmail.com <ssimonrubinstein1@gmail.com@37c11c60-c4e7-ad1f-c740-338379875b08>
2012-03-02 12:16:29 +0000

    in progress

---
+++ trunk/scripts/bot-test.pl
@@ -0,0 +1,81 @@
+#!/usr/bin/perl
+# @created 2012-02-25
+# @date 2012-02-25
+# @author Simon Rubinstein <ssimonrubinstein1@gmail.com>
+# http://code.google.com/p/cocobot/
+#
+# copyright (c) Simon Rubinstein 2010-2012
+# Id: $Id$
+# Revision: $Revision$
+# Date: $Date$
+# Author: $Author$
+# HeadURL: $HeadURL$
+#
+# cocobot is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# cocobot is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA  02110-1301, USA.
+use strict;
+use warnings;
+use FindBin qw($Script $Bin);
+use Data::Dumper;
+use utf8;
+no utf8;
+use lib "../lib";
+use Cocoweb;
+use Cocoweb::CLI;
+my $CLI;
+
+init();
+run();
+
+sub run {
+    my $bot = $CLI->getBot('generateRandom' => 1);
+    $bot->process();
+    $bot->show();
+    info("The $Bin script was completed successfully.");
+}
+
+## @method void init()
+sub init {
+    $CLI = Cocoweb::CLI->instance();
+    my $opt_ref = $CLI->getOpts();
+    if ( !defined $opt_ref ) {
+        HELP_MESSAGE();
+        exit;
+    }
+}
+
+## @method void HELP_MESSAGE()
+# Display help message
+sub HELP_MESSAGE {
+    print <<ENDTXT;
+Usage: 
+ $Script [-v -d -a myavatar -p mypass]
+  -a myavatar
+  -p mypass
+  -v          Verbose mode
+  -d          Debug mode
+ENDTXT
+    exit 0;
+}
+
+## @method void VERSION_MESSAGE()
+sub VERSION_MESSAGE {
+    print STDOUT <<ENDTXT;
+    $Script $Cocoweb::VERSION (2012-02-24) 
+     Copyright (C) 2010-2012 Simon Rubinstein 
+     Written by Simon Rubinstein 
+ENDTXT
+}
+
