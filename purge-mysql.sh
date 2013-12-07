d3b4b403121a7157716c16a9b90e354d216c404b
ssimonrubinstein1 <ssimonrubinstein1@37c11c60-c4e7-ad1f-c740-338379875b08>
2013-12-07 20:51:29 +0000

    in progess

--- purge-mysql.sh
+++ purge-mysql.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 mysql -u test -pGim9p6gW test -e "DELETE FROM users WHERE id_ISP  IN (SELECT id from ISPs WHERE name = 'euNetworks Services GmbH');"
-#mysql -u test -pGim9p6gW test -e "DELETE FROM users where id_code IN (SELECT ID FROM codes WHERE code IN ('19f', 'rYK'));" 
+mysql -u test -pGim9p6gW test -e "DELETE FROM users where id_code IN (SELECT ID FROM codes WHERE code IN ('kVO', '6eF'));" 
 mysql -u test -pGim9p6gW test -e "DELETE FROM users WHERE update_date <= DATE_SUB(CURRENT_TIMESTAMP(), INTERVAL 28 DAY) AND mysex IN (1,6);"
 mysql -u test -pGim9p6gW test -e "DELETE FROM users WHERE id_code IN (SELECT id FROM codes WHERE update_date <= DATE_SUB(CURRENT_TIMESTAMP(), INTERVAL 18 DAY)) AND mysex IN (1, 6);"
 mysql -u test -pGim9p6gW test -e "DELETE FROM users WHERE id_code IN (SELECT id FROM codes WHERE update_date <= DATE_SUB(CURRENT_TIMESTAMP(), INTERVAL 30 DAY)) AND mysex IN (2, 7);"
 mysql -u test -pGim9p6gW test -e "DELETE FROM codes WHERE id NOT IN (SELECT distinct id_code from users);"
 mysql -u test -pGim9p6gW test -e "DELETE FROM nicknames WHERE id NOT IN (SELECT distinct id_mynickname from users);"
+
+mysql -u test -pGim9p6gW test -e "SELECT COUNT(*) FROM codes; SELECT COUNT(*) FROM users; SELECT COUNT(*) FROM nicknames;"
