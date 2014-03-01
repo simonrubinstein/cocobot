b698daa0bc231d51f4f1a9c154d514e862ebd2fa
ssimonrubinstein1 <ssimonrubinstein1@37c11c60-c4e7-ad1f-c740-338379875b08>
2014-03-01 18:37:20 +0000

    add

--- scripts/list-users.pl
+++ scripts/list-users.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 # @brief Displays the list of users logged on the website Coco.fr
 # @created 2012-02-22
-# @date 2012-03-29
+# @date 2014-03-01
 # @author Simon Rubinstein <ssimonrubinstein1@gmail.com>
 # http://code.google.com/p/cocobot/
 #
@@ -36,6 +36,7 @@ use lib "../lib";
 use Cocoweb;
 use Cocoweb::CLI;
 my $CLI;
+my %nicknames2filter = ();
 
 init();
 run();
@@ -45,15 +46,18 @@ sub run {
     my $bot = $CLI->getBot( 'generateRandom' => 1 );
     $bot->requestAuthentication();
     $bot->display();
+
+    # Get a 'Cocoweb::User::List' object
     my $usersList = $bot->requestUsersList();
     if ( !defined $usersList ) {
         warning("No users found");
     }
     else {
         $usersList->display(
-            'mysex'     => $CLI->mysex(),
-            'myage'     => $CLI->myage(),
-            'mynickame' => $CLI->searchNickname()
+            'mysex'            => $CLI->mysex(),
+            'myage'            => $CLI->myage(),
+            'mynickame'        => $CLI->searchNickname(),
+            'nicknames2filter' => \%nicknames2filter
         );
     }
     info("The $Bin script was completed successfully.");
@@ -62,10 +66,22 @@ sub run {
 ##@method void init()
 sub init {
     $CLI = Cocoweb::CLI->instance();
-    my $opt_ref = $CLI->getOpts( 'argumentative' => 'l:' );
+    my $opt_ref = $CLI->getOpts( 'argumentative' => 'l:f:' );
     if ( !defined $opt_ref ) {
         HELP_MESSAGE();
         exit;
+    }
+    my $filtersStr = $opt_ref->{'f'} if exists $opt_ref->{'f'};
+    if ( defined $filtersStr ) {
+        my @filters = split( /,/, $filtersStr );
+        foreach my $f (@filters) {
+            my $file = Cocoweb::Config->instance()
+                ->getConfigFile( $f, 'Plaintext' );
+            my $lines_ref = $file->getAll();
+            foreach my $nickname (@$lines_ref) {
+                $nicknames2filter{$nickname} = 1;
+            }
+        }
     }
 }
 
@@ -73,19 +89,22 @@ sub init {
 # Display help message
 sub HELP_MESSAGE {
     print STDOUT $Script
-      . ', displays the list of users logged on the website Coco.fr' . "\n";
+        . ', displays the list of users logged on the website Coco.fr' . "\n";
     $CLI->printLineOfArgs('-l nickmaneWanted');
     print STDOUT '  -l nickmaneWanted Nickname that will be '
-      . "filtered to display the list.\n";
+        . "filtered to display the list.\n";
+    print STDOUT '  -f filters'
+        . '        Filters (i.e. plain-text/nicknames-to-filter.txt)' . "\n";
     $CLI->HELP();
     print STDOUT '  The arguments -s (sex) and -y (age) are also used'
-      . ' to filter the display of the list.' . "\n";
+        . ' to filter the display of the list.' . "\n";
+
     exit 0;
 }
 
 ##@method void VERSION_MESSAGE()
 #@brief Displays the version of the script
 sub VERSION_MESSAGE {
-    $CLI->VERSION_MESSAGE('2012-03-29');
+    $CLI->VERSION_MESSAGE('2014-03-01');
 }
 
